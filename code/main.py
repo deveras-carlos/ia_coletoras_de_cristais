22b53729eb62843c254c85a88a88529b45be3641
Leandro Iglesias <leandro.iglesias@nca.ufma.br>
2024-11-25 17:27:14 -0300

    feat(Leandro): Adicionando um método para parar a simulação

--- code/main.py
+++ code/main.py
@@ -3,12 +3,12 @@ import pygame
 
 from simulacao import Simulacao
 from parametros import Parametro
-
+Stop_simulation=False
 if __name__ == "__main__":
     pygame.init(  )
     pygame.mixer.init(  )
-    SCREEN_WIDTH = 1280
-    SCREEN_HEIGHT = 720
+    SCREEN_WIDTH = 120
+    SCREEN_HEIGHT = 120
     FPS = 1
     clock = pygame.time.Clock(  )
     screen = pygame.display.set_mode( ( SCREEN_WIDTH, SCREEN_HEIGHT ) )
@@ -18,11 +18,16 @@ if __name__ == "__main__":
 
     while True:
         for event in pygame.event.get(  ):
+        
+            if event.type == pygame.KEYDOWN:
+
+                Stop_simulation=not Stop_simulation
             if event.type == pygame.QUIT:
                 pygame.quit(  )
                 sys.exit(  )
         
-        simulacao.run(  )
+        if not Stop_simulation:
+            simulacao.run(  )
 
         pygame.display.update(  )
         clock.tick( FPS )
